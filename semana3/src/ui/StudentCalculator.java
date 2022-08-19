a2c993d6f4de3a57dd7f4ad4f574d010aa0ea544
njse22 <njse22@gmail.com>
2022-08-19 12:12:16 -0500

    update semana 3

--- semana3/src/ui/StudentCalculator.java
+++ semana3/src/ui/StudentCalculator.java
@@ -1,18 +1,23 @@
 import java.util.Scanner; 
+import java.lang.Math;
 
 public class StudentCalculator{
 
-	public static void main(String[] args){
+	public static void main(String[] args){ // inicio del método main
 			
 		Scanner reader = new Scanner(System.in);
 
 		int option = 0; 
 
-		do{
-			System.out.println("seleccione un menu: \n" +
+		do{ // inicio del ciclo
+			System.out.println("seleccione una opción: \n" +
 					"option 1 \n"+
-					"option 2 \n");
+					"option 2 \n"+ 
+					"option 3: calculate attenuation.");
+			// leer la opción ingresada por el usuario. 
 			option = reader.nextInt(); 
+
+			// estructura de control 
 			switch(option){
 				case 1:
 					System.out.println("este es la option 1"); 
@@ -22,6 +27,16 @@ public class StudentCalculator{
 					System.out.println("This is the option 2"); 
 					break; 
 
+				case 3: 
+					System.out.println("Calculate Attenuation: "); 
+					System.out.println("Ingrese la distancia entre los dos puntos");
+					double distance = reader.nextDouble();
+					System.out.println("Ingrese la longitud de onda de la señal: "); 
+					double lambda = reader.nextDouble(); 
+					double lbf = calculateAtenuation(distance, lambda); 
+					System.out.println("el resultado de la atenuación es: " + lbf + "dB"); 
+					break;
+			  // finaliza el ciclo while
 				case 0:
 					System.out.println("Bye !!!");
 					break;
@@ -29,9 +44,20 @@ public class StudentCalculator{
 				default:
 					System.out.println("Invalid option.");
 			}
-		}while(option != 0);
+		}while(option != 0); // fin del ciclo
 
+	} // fin del método main 
+
+	/*
+	 *
+	 * */
+	public static double calculateAtenuation(double distance, double lambda){
+		double lbf = 0.0; 
+		lbf = (Math.log10(4*Math.PI*distance))/lambda;
+		return lbf; 
 	}
+
+
 
 
 }
