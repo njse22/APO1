0fb5905007ddf0a42a5ce21ecb598862db71f5d4
Nicolás Javier Salazar Echeverry <38898558+njse22@users.noreply.github.com>
2022-10-27 15:10:20 -0500

    add factory animals example

--- semana13/Evolution/src/model/AnimalController.java
+++ semana13/Evolution/src/model/AnimalController.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 public class AnimalController {
 
 	private ArrayList<Animal> animals; 
-	private AnimalType animalType; 
 
 	public AnimalController() {
 		animals = new ArrayList<Animal>(20);
-
 	}	
 
 	public ArrayList<Animal> getAnimals(){
@@ -36,16 +34,65 @@ public class AnimalController {
 		return msj;
 	}
 
-	public void factoryAnimal(double weight, double height, int age, String name,
+	public void factoryAnimalWithEnumeration(double weight, double height, int age, String name,
 			boolean isDomestic, Species species, 
 			double wingSpan, AnimalType animalType){
 
+		Animal newAnimal = null; 
 		switch(animalType){
-			case 
+			case AnimalType.HUMAN:
+				newAnimal = new Human(weight, height, age, name); 
+			break;
+
+			case AnimalType.FELINDAE:
+				newAnimal = new Felidae(weight, height, age, name, species); 
+			break; 
+
+			case AnimalType.CHICKEN:
+				newAnimal= new Chicken(weight, height, age, wingSpan); 
+			break; 
+
+			case AnimalType.DUCK:
+				newAnimal= new Duck(weight, height, age, wingSpan); 
+			break; 
 
 		}
 
+		addAnimal(newAnimal); 
+
 	}
+
+	public String factoryAnimalWithString(double weight, double height, int age, String name,
+			boolean isDomestic, Species species, 
+			double wingSpan, String animalType){
+
+		Animal newAnimal = null;
+		String msj = ""; 
+
+		if(animalType.equalsIgnoreCase("humano")){
+			newAnimal = new Human(weight, height, age, name); 
+			msj = "nuevo humano creado"
+		}
+		else if(animalType.equalsIgnoreCase("felino")){
+			newAnimal = new Felidae(weight, height, age, name, species);
+			msj = "nuevo felino creado" 
+		}
+		else if(animalType.equalsIgnoreCase("pollo")){
+			newAnimal= new Chicken(weight, height, age, wingSpan); 
+			msj = "nuevo pollo creado"
+		}
+		else if(animalType.equalsIgnoreCase("pato")){
+			newAnimal= new Duck(weight, height, age, wingSpan); 
+			msj = "nuevo pato creado"
+		}
+		else {
+			msj = "Escriba un tipo de animal valido"; 
+		}
+
+		addAnimal(newAnimal); 
+		return msj; 
+	}
+
 	
 	public String printArray(){
 		String msj = "";
